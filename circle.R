b28b3d135d53e85a0f1034d7cee80ccc6385fe0d
emittman <emittman@iastate.edu>
2017-02-23 16:21:58 -0600

    Set verbose=0

--- circle.R
+++ circle.R
@@ -19,6 +19,6 @@ y <- t(apply(betas, 2, function(b) rnorm(2*reps, X%*%b, sigma_e)))
 data <- formatData(y, X, transform_y=identity)
 priors <- formatPriors(K = G, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 1, a = 1, b = 1)
 
-out <- mcmc(data, priors, n_iter = 1000, idx_save = 0:(G-1), thin = 1, verbose = 3)
+out <- mcmc(data, priors, n_iter = 1000, idx_save = 0:(G-1), thin = 1, verbose = 0)
 
 saveRDS(out, file="circle_samples.rds")
