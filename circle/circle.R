8c4e1e3b477d7dbe779013156607c7e0abc2c1d7
emittman <emittman@iastate.edu>
2017-02-23 17:10:10 -0600

    Informative prior on tau2

--- circle/circle.R
+++ circle/circle.R
@@ -17,9 +17,9 @@ X <- kronecker(diag(2), rep(1, reps))
 y <- t(apply(betas, 2, function(b) rnorm(2*reps, X%*%b, sigma_e)))
 
 data <- formatData(y, X, transform_y=identity)
-priors <- formatPriors(K = 50, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 1, a = 1, b = .5)
+priors <- formatPriors(K = 50, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 1, a = 2, b = 2)
 
-out <- mcmc(data, priors, n_iter = 10000, idx_save = 0:(G-1), thin = 1, verbose = 0)
+out <- mcmc(data, priors, n_iter = 10000, idx_save = 0:(G-1), thin = 10, verbose = 0)
 
 saveRDS(out, file="circle_samples.rds")
 saveRDS(list(truth = betas, data = data, priors = priors), file="circle_truth.rds")
--- circle/circle_check.R
+++ circle/circle_check.R
@@ -1,9 +1,23 @@
 s <- readRDS("circle_samples.rds")
-
+t <- readRDS("circle_truth.rds")
 library(coda)
-mcmcs <- lapply(1:10, function(g) mcmc(s$beta[1,,g]))
+mcmcs <- lapply(1:10*20, function(g) mcmc(s$beta[1,,g]))
+truths <- t$truth[1, 1:10*20]
 par(mfrow=c(3,4))
 for(g in 1:10){
-  traceplot(mcmcs[[g]], ylim=c(-10,10))
+  traceplot(mcmcs[[g]], ylim=c(-15,15))
+  abline(h=truths[g], col="red")
 }
 dev.off()
+
+g_ran <- sample(200, 1)
+par(mfrow=c(2,1))
+plot(t(s$beta[,1:5000,g_ran]))
+points(t(t$truth[,g_ran]), col="red")
+plot(t(s$beta[,5001:10000,g_ran]))
+points(t(t$truth[,g_ran]), col="red")
+dev.off()
+
+sd(s$beta[,5001:10000,g_ran])
+
+plot(s$beta[1,5001:10000,g_ran], s$tau2[g_ran,5001:10000])
