1f226a1acbb77b1dd13ca2ae675e9e30350445e2
emittman <emittman@iastate.edu>
2017-02-24 09:37:23 -0600

    "Bound" tau2 further from zero

--- circle/circle.R
+++ circle/circle.R
@@ -17,7 +17,7 @@ X <- kronecker(diag(2), rep(1, reps))
 y <- t(apply(betas, 2, function(b) rnorm(2*reps, X%*%b, sigma_e)))
 
 data <- formatData(y, X, transform_y=identity)
-priors <- formatPriors(K = 200, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 5, a = 2, b = 2)
+priors <- formatPriors(K = 200, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 5, a = 3, b = 2)
 
 out <- mcmc(data, priors, n_iter = 10000, idx_save = 0:(G-1), thin = 10, verbose = 0)
 
