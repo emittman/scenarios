74341018903108e9df83a62ace2932303f907bb2
emittman <emittman@iastate.edu>
2017-02-24 09:05:52 -0600

    OK

--- circle/circle_check.R
+++ circle/circle_check.R
@@ -12,12 +12,12 @@ dev.off()
 
 g_ran <- sample(200, 1)
 par(mfrow=c(2,1))
-plot(t(s$beta[,1:5000,g_ran]))
+plot(t(s$beta[,1:500,g_ran]))
 points(t(t$truth[,g_ran]), col="red")
-plot(t(s$beta[,5001:10000,g_ran]))
+plot(t(s$beta[,501:1000,g_ran]))
 points(t(t$truth[,g_ran]), col="red")
 dev.off()
 
-sd(s$beta[,5001:10000,g_ran])
+sd(s$beta[,501:1000,g_ran])
 
-plot(s$beta[1,5001:10000,g_ran], s$tau2[g_ran,5001:10000])
+plot(s$beta[1,501:1000,g_ran], s$tau2[g_ran,501:1000])
