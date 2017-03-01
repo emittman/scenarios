b4cfbb7c973c7eb56399085ee4c49645199b7b01
emittman <emittman@iastate.edu>
2017-03-01 13:23:14 -0600

    OK

--- circle/circle.R
+++ circle/circle.R
@@ -19,7 +19,7 @@ y <- t(apply(betas, 2, function(b) rnorm(2*NperV, X%*%b, sigma_e)))
 data <- formatData(y, X, transform_y=identity)
 priors <- formatPriors(K = G, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 5, a = 3, b = 2)
 idx_save = 0:9 * 10
-out <- mcmc(data, priors, n_iter = 1000, idx_save = idx_save, thin = 1, verbose = 0)
+out <- mcmc(data, priors, n_iter = 10000, idx_save = idx_save, thin = 1, verbose = 0)
 
-saveRDS(out, file="circle_samples.rds")
+saveRDS(out$beta[,1001:10000,], file="circle_samples.rds")
 saveRDS(list(truth = betas, data = data, priors = priors), file="circle_truth.rds")
