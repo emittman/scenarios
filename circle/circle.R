b321ea1543cf55c54a03fe669b6400a3e687492f
emittman <emittman@iastate.edu>
2017-03-01 12:26:57 -0600

    Run 1000 iterations, G=K=100

--- circle/circle.R
+++ circle/circle.R
@@ -2,13 +2,13 @@ library(cudarpackage)
 
 set.seed(22317)
 
-G <- 1000
+G <- 100
 
-reps <- 4
+N <- 10
 
 sigma0 <- 5
 
-sigma_e <- .1
+sigma_e <- 1
 
 betas <- sapply(1:G, function(g) c(sigma0 * cos(g*2*pi/G), sigma0 * sin(g*2*pi/G)))
 
@@ -17,9 +17,9 @@ X <- kronecker(diag(2), rep(1, reps))
 y <- t(apply(betas, 2, function(b) rnorm(2*reps, X%*%b, sigma_e)))
 
 data <- formatData(y, X, transform_y=identity)
-priors <- formatPriors(K = 20, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 5, a = 3, b = 2)
-
-out <- mcmc(data, priors, n_iter = 10000, idx_save = 0:9 * 100, thin = 1, verbose = 0)
+priors <- formatPriors(K = G, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 5, a = 3, b = 2)
+idx_save = 0:9 * 10
+out <- mcmc(data, priors, n_iter = 1000, idx_save = idx_save, thin = 1, verbose = 0)
 
 saveRDS(out, file="circle_samples.rds")
 saveRDS(list(truth = betas, data = data, priors = priors), file="circle_truth.rds")
