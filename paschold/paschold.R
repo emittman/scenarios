e639217e3296f1b8ba37a9afd50c29ce2661c3a0
emittman <emittman@iastate.edu>
2017-03-07 13:30:40 -0600

    Try out mcmc() on Paschold2012 data

---
+++ paschold/paschold.R
@@ -0,0 +1,65 @@
+library(cudarpackage)
+library(Paschold2012)
+library(dplyr)
+
+str(Paschold2012)
+counts = Paschold2012 %>%
+  mutate(genotype_replicate = paste(genotype,replicate,sep="_")) %>%
+  select(GeneID, genotype_replicate, total) %>%
+  tidyr::spread(genotype_replicate, total)
+
+zero_rows <- counts$GeneID[which(sapply(1:nrow(counts), function(row) sum(counts[row,-1]) == 0))]
+y <- filter(counts, !(GeneID %in% zero_rows)) %>%
+  select(-GeneID) %>%
+  as.matrix()
+
+log_yp1 <- log(y+1)
+
+X <- matrix(c(1, 1, 0, 0,
+              1, 0, 1, 1,
+              1, -1, 0, 0,
+              1, 0, 1, -1), 4, 4, byrow=T)
+groups <- rep(1:4, each=4)
+
+data <- formatData(counts=y, X=X, groups = groups)
+
+ols <- lapply(1:nrow(log_yp1), function(g){
+  fit <- lm(log_yp1[g,] ~ 0 + X[groups,])
+  out <- c(coef(fit), sigma(fit))
+  names(out) <- c("parent", "parent_hd", "hybrid", "hybrid_hd", "sigma")
+  out  
+})
+
+ols <- do.call("rbind", ols)
+
+#set priors for beta_k: multiply by 1.5 sd of ols, set prior mean to median of ols
+mu_0 <- sapply(1:4, function(v) median(ols[,v]))
+sigma_0 <- sapply(1:4, function(v) 1.5 *sd(ols[,v]) )
+
+#set priors for tau_k: method of moments based on ols estimates of sigma_g
+pr_tau2_var <- var(1/(ols[,'sigma']^2+.01)) #increase variance estimates slightly for stability
+pr_tau2_mean <- mean(1/(ols[,'sigma']^2+.01))
+a <- pr_tau2_mean^2 / pr_tau2_var
+b <- pr_tau2_mean / pr_tau2_var
+
+priors <- formatPriors(K=2000, prior_mean = mu_0, prior_sd = sigma_0, alpha = 20, a = a, b = b)
+init_chain <- function(priors, G){
+  beta <- with(priors, matrix(rnorm(V*K, rep(priors$mu_0, K), rep(sqrt(1/lambda2), K)), V, K))
+  tau2 <- with(priors, rgamma(K, a, b))
+  pi <- with(priors, rep(1/K, K))
+  zeta <- with(priors, as.integer(sample(K, G, replace=T) - 1))
+  formatChain(beta, pi, tau2, zeta)
+}
+
+
+chain <- init_chain(priors, data$G)
+chain$C <- matrix(c(0, -1, 1, 1, # heterosis
+                    0, -1, 1, -1,
+                    0, 1, 1, 1,
+                    0, 1, 1, -1), 4, 4, byrow=T)
+chain$P <- 4 #nrow(C). Redundant, since P = V = 4 by default
+
+idx_save <- sample(data$G, 20) - 1
+
+out <- mcmc(data, priors, chain, n_iter = 10, n_save_P = 1000, idx_save = idx_save, thin = 1, verbose = 0)
+saveRDS(out, "output_paschold.rds")
---
+++ small_circle/small_circle_check.R
@@ -0,0 +1,138 @@
+s <- readRDS("small_circle_samples.rds")
+t <- readRDS("small_circle_truth.rds")
+
+source("constants.R")
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(coda)
+
+t$mles <- with(t, data$xty/(data$N/data$V))
+t$est_prec <- with(t, sapply(1:G, function(g){
+  data$N / (data$yty[g] + sum(mles[,g]^2) * data$N/data$V - 2 * t(mles[,g]) %*% data$xty[,g])
+}))
+mcmcs <- lapply(1:length(idx_save), function(g) mcmc(s[[1]]$beta[1,,g]))
+truths <- with(t, truth[1,zeta+1])
+opar = par(mfrow=c(3,4))
+for(g in 1:12){
+  traceplot(mcmcs[[g]])
+  abline(h=t$mles[1, idx_save[g]+1], col="red")
+  abline(h=truths[g], col="blue", lty="dashed")
+}
+gpar <- par(opar) # reset plot grid
+opar <- par(gpar)
+
+mcmcs <- lapply(1:length(idx_save), function(g) mcmc(s[[1]]$tau2[g,]))
+for(g in 1:12){
+  traceplot(mcmcs[[g]])
+  abline(h=t$est_prec[idx_save[g]+1], col="red")
+  abline(h=1/sigma_e^2, col="blue", lty=2)
+}
+gpar <- par(opar)
+
+g_ran <- sample(length(idx_save), 1)
+opar <- par(mfrow=c(2,1))
+plot(t(s[[1]]$beta[,1:5000,g_ran]))
+with(t, points(t(truth[,zeta[g_ran]+1]), col="blue"))
+points(t(t$mles[,idx_save[g_ran]+1]), col="red")
+plot(t(s[[1]]$beta[,5001:10000,g_ran]))
+with(t, points(t(truth[,zeta[g_ran]+1]), col="blue"))
+points(t(t$mles[,idx_save[g_ran]+1]), col="red")
+ppar <- par(opar)
+
+apply(s[[1]]$beta[,5001:10000,g_ran], 1, function(b) quantile(b, c(.01,.5,.99)))
+with(t, truth[,zeta[g_ran]+1])
+quantile(s[[1]]$tau2[g_ran, 1:2000], c(.01, .5, .99))
+data.frame(beta=s[[1]]$beta[1,5001:10000,g_ran], tau2=s[[1]]$tau2[g_ran,5001:10000]) %>%
+  ggplot(aes(beta, tau2)) + geom_hex(bins=30) +
+  geom_point(data=data.frame(b0=0, t0=1), inherit.aes = F, aes(x=b0,y=t0), color="blue")+
+  geom_point(data=data.frame(b0=t$mles[1,g_ran], t0=t$est_prec[g_ran]), inherit.aes=F,
+             aes(x=b0,y=t0), color="red")
+
+# weighted histograms
+P <- with(s[[1]], data.frame(x1=as.numeric(P[,2,251:500]),
+                             x2=as.numeric(P[,3,251:500]),
+                             w=as.numeric(P[,1,251:500])))
+true_means <- data.frame(x0=0, y0=0, r=5)
+
+P %>%
+ggplot(aes(x1, x2, weight=w)) + geom_hex(bins=25) + scale_fill_continuous()
+
+
+#occupancy
+opar <- par(ppar)
+plot(prop.table(table(s[[1]]$max_id[5001:10000]+1)), main="max id", ylab="")
+plot(prop.table(table(s[[1]]$num_occupied[5001:1000])), main="occupied clusters", ylab="")
+ppar <- par(opar)
+#check_probs
+probs.df <- plyr::ldply(1:25, function(g){
+  extern = mean(s[[1]]$beta[1,,g]>0 & s[[1]]$beta[2,,g]>0)
+  intern = s[[2]]$probs[g]
+  data.frame(extern=extern, intern=intern)
+})
+
+with(probs.df, all.equal(extern, intern))
+
+#means
+mean.mle <- data.frame(g = 1:25, group="mle", t(t$mles))
+names(mean.mle)[3:4] <- c("beta1", "beta2")
+mean.mle <- gather(mean.mle, key="component", value="value", 3:4)
+
+mean.mcmc <- data.frame(g=1:25, group="mcmc", t(s[[2]]$means))
+names(mean.mcmc)[3:4] <- c("beta1", "beta2")
+mean.mcmc <- gather(mean.mcmc, key="component", value="value", 3:4)
+
+mean.truth <- data.frame(g=1:25, t(t$truth[,t$zeta+1]))
+names(mean.truth)[2:3] <- c("beta1", "beta2")
+mean.truth <- gather(mean.truth, key="component", value="true_value", 2:3)
+
+mean.df <- merge(mean.truth, rbind(mean.mle, mean.mcmc), by=c("g", "component"))
+
+ggplot(mean.df, aes(x=factor(round(true_value, 1)), y=value, fill=group)) + 
+  geom_boxplot() +
+  theme_bw() +
+  scale_y_continuous(breaks=c(-5,0,5)) +
+  facet_wrap(~component)
+  ggplot(mean.df, aes(x=beta1.truth, y=beta1.mcmc)) + geom_point()
+
+#normal approximation
+approx.mcmc <- with(s[[2]], plyr::ldply(1:25, function(g){
+  mean <- means[,g]
+  vars <- meansquares[,g] - means[,g]^2
+  df <- data.frame(g=g, component=c("beta1","beta2"), mean = mean, sd = sqrt(vars))
+}))
+
+pdf("beta1_posteriors.pdf")
+par(mfrow=c(5,5),
+    oma = c(2, 2, 0, 0), # two rows of text at the outer left and bottom margin
+    mar = c(1, 1, 0, 0), # space for one row of text at ticks and to separate plots
+    mgp = c(2, 1, 0))    # axis label at 2 rows distance, tick labels at 1 row
+
+for(i in 1:25){
+  hist(s[[1]]$beta[1,5001:10000,i], 100, prob=T, axes=F, main="")
+  with(filter(approx.mcmc, g==i & component=="beta1"),
+       curve(dnorm(x, mean, sd), add=T, lty=2, col=3))
+  axis(side=1, at=-6:6, cex.axis=.7)
+}
+title(xlab = "value",
+      ylab = "density",
+      outer = TRUE, line = 3)
+dev.off()
+
+pdf("beta2_posteriors.pdf")
+par(mfrow=c(5,5),
+    oma = c(2, 2, 0, 0), # two rows of text at the outer left and bottom margin
+    mar = c(1, 1, 0, 0), # space for one row of text at ticks and to separate plots
+    mgp = c(2, 1, 0))    # axis label at 2 rows distance, tick labels at 1 row
+
+for(i in 1:25){
+  hist(s[[1]]$beta[2,5001:10000,i], 100, prob=T, axes=F, main="")
+  with(filter(approx.mcmc, g==i & component=="beta2"),
+       curve(dnorm(x, mean, sd), add=T, lty=2, col=3))
+  axis(side=1, at=-6:6, cex.axis=.7)
+}
+title(xlab = "value",
+      ylab = "density",
+      outer = TRUE, line = 3)
+dev.off()
