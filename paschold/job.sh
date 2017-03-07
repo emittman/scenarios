922494f56eeed919504556472c522dbeb8a96b89
emittman <emittman@iastate.edu>
2017-03-07 14:41:08 -0600

    Try reserving full node

--- paschold/job.sh
+++ paschold/job.sh
@@ -3,11 +3,12 @@
 #PBS  -o BATCH_OUTPUT 
 #PBS  -e BATCH_ERRORS 
 
-#PBS -lnodes=1:ppn=1:gpu,walltime=4:00:00
+#PBS -lnodes=1:ppn=16:gpu,walltime=4:00:00
 
 # Change to directory from which qsub was executed 
 cd $PBS_O_WORKDIR
 
-module load R
 
-R CMD BATCH --vanilla paschold.R
+#Note: Reserve full nodes when using the gpu(s)
+module load R
+R CMD BATCH --vanilla paschold.R 
