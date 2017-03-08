b34fb7cd92070d5ac989c2979f2ecd8fde0fdeb4
emittman <emittman@iastate.edu>
2017-03-08 09:39:36 -0600

    Use srun and not salloc

--- paschold/job.sh
+++ paschold/job.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-salloc -N 1 -n 16 -p gpu -t 4:00:00
 
-# Change to directory from which qsub was executed 
-cd $PBS_O_WORKDIR
 module load R
-srun R --vanilla CMD BATCH paschold.R
+srun -N 1 -n 16 -p gpu -t 4:00:00 R --vanilla CMD BATCH paschold.R
