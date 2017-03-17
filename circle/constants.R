82ad73efbc6f623a1a090d2afde99814619b2947
emittman <emittman@iastate.edu>
2017-03-17 15:15:01 -0500

    Do first several iterations for SD model on large data set

--- circle/constants.R
+++ circle/constants.R
@@ -1,5 +1,5 @@
 #constants for circle scenario
-G <- 5000
+G <- 1000
 
 NperV <- 5
 
@@ -9,8 +9,8 @@ sigma_e <- .7
 
 idx_save = 0:99 * 50
 
-n_iter <- 10000
+n_iter <- 3
 
-n_save_P <- 1000
+n_save_P <- 1
 
 thin <- 1
