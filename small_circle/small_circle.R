10665a7b3fd6382fddd98de3530090134a43dc5f
emittman <emittman@iastate.edu>
2017-03-24 12:52:03 -0500

    Do not make verbose

--- small_circle/small_circle.R
+++ small_circle/small_circle.R
@@ -18,13 +18,13 @@ C <- list(matrix(c(1, 0), 1, 2),
 data <- formatData(y, X, transform_y=identity)
 priors <- formatPriors(K = G, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 1, a = 3, b = 2)
 out1 <- mcmc(data, priors, methodPi="stickBreaking", n_iter = n_iter,
-            n_save_P=1, idx_save = idx_save, thin = 1, verbose =3, C=C)
+            n_save_P=1, idx_save = idx_save, thin = 1, verbose =0, C=C)
 out2 <- mcmc(data, priors, methodPi="symmDirichlet", n_iter = n_iter,
              n_save_P=n_save_P, idx_save = idx_save, thin = 1, verbose = 0, C=C)
 out3 <- mcmc(data, priors, methodPi="stickBreaking", alpha_fixed = F, n_iter = n_iter,
-             n_save_P=1, idx_save = idx_save, thin = 1, verbose =3, C=C)
+             n_save_P=1, idx_save = idx_save, thin = 1, verbose =0, C=C)
 out4 <- mcmc(data, priors, methodPi="symmDirichlet", alpha_fixed = F, n_iter = n_iter,
-            n_save_P=1, idx_save = idx_save, thin = 1, verbose =3, C=C)
+            n_save_P=1, idx_save = idx_save, thin = 1, verbose =0, C=C)
 saveRDS(out1, file="small_circle_samples_sb.rds")
 saveRDS(out2, file="small_circle_samples_sd.rds")
 saveRDS(list(truth = betas, zeta = zeta, data = data, priors = priors), file="small_circle_truth.rds")
