f3e735c6e2d2f117b0ce31e9adff840fc5270f1b
emittman <emittman@iastate.edu>
2017-03-24 22:40:16 -0500

    Try this experiment again

--- circle/circle.R
+++ circle/circle.R
@@ -11,7 +11,8 @@ X <- kronecker(diag(2), rep(1, NperV))
 y <- t(apply(betas, 2, function(b) rnorm(2*NperV, X%*%b, sigma_e)))
 
 data <- formatData(y, X, transform_y=identity)
-priors <- formatPriors(K = G, prior_mean = rep(0,2), prior_sd = rep(sigma0,2), alpha = 10, a = 3, b = 2)
+priors <- formatPriors(K = 500, prior_mean = rep(0,2), prior_sd = rep(sigma0,2),
+                       alpha = 10, a = 3, b = 2, A = 10, B = 1)
 
 out <- mcmc(data, priors, methodPi="stickBreaking", n_iter = n_iter, n_save_P = n_save_P,
             idx_save = idx_save, thin = thin, verbose = 0)
