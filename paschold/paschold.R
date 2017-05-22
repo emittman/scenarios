b43352c91cebd129d4b43be09256a1d6dbb4264d
emittman <emittman@iastate.edu>
2017-05-22 16:22:06 -0500

    fix typo

--- paschold/paschold.R
+++ paschold/paschold.R
@@ -21,8 +21,8 @@ C <- list(hph = matrix(c(0, -1, 1, 0,
                          0, 1,  1, 0),2,4, byrow=T),
           lph = matrix(c(0, -1, -1, 0,
                          0, 1, -1, 0),2,4, byrow=T),
-          hybpos = matrix(c(0, 0, 1, 0),1,2, byrow=T),
-          hybneg = matrix(c(0, 0, -1, 0),1, 2, byrow=T)) 
+          hybpos = matrix(c(0, 0, 1, 0),1,4, byrow=T),
+          hybneg = matrix(c(0, 0, -1, 0),1, 4, byrow=T)) 
 
 idx_save <- 1:10*200-1
 n_iter <- 30000
